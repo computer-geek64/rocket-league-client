c4adb481693894536492dc763c3f933f2c004a65
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2018-09-09 13:57:02 -0400

    Update Update.py

--- Update.py
+++ Update.py
@@ -6,7 +6,7 @@ import os
 import Git
 
 
-repository_name = os.getcwd().split("/win")[0]
+repository_name = os.getcwd()
 Git.fetch(repository_name)
 if not Git.is_up_to_date(repository_name):
     Git.pull(repository_name)

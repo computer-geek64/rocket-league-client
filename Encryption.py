2f8ecc03aa04e200bf164210dd3ce9e62705122f
koastiebratt <koastiebratt@gmail.com>
2018-09-09 11:41:12 -0700

    Update

--- Encryption.py
+++ Encryption.py
@@ -8,11 +8,11 @@ import base64
 
 def encrypt(text, passphrase):
     IV = "1234567890123456"
-    aes = AES.new(passphrase, AES.MODE_CFB, IV)
-    return base64.b64encode(aes.encrypt(text)).decode("ascii")
+    aes = AES.new(str.encode(passphrase), AES.MODE_CFB, str.encode(IV))
+    return base64.b64encode(aes.encrypt(str.encode(text))).decode("ascii")
 
 
 def decrypt(cipher_text, passphrase):
     IV = "1234567890123456"
-    aes = AES.new(passphrase, AES.MODE_CFB, IV)
+    aes = AES.new(str.encode(passphrase), AES.MODE_CFB, str.encode(IV))
     return aes.decrypt(base64.b64decode(cipher_text)).decode("ascii")
